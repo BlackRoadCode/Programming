dec52f881a2b2ebc3ca31a179e56acd2c7d3d3c2
BlackRoadCode <blackroadcode@gmail.com>
2022-04-15 21:58:03 -0500

    end of section 6

--- curso-u-react-cero-experto/04-gif-expert-app/src/components/GifGrid.js
+++ curso-u-react-cero-experto/04-gif-expert-app/src/components/GifGrid.js
@@ -1,33 +1,21 @@
-// import { useEffect, useState } from "react";
-
-// import { getGifs } from "../helpers/getGifs";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ( { category } ) => {
 
-    // const [images, setimages] = useState([]);
-
-    const { loading } = useFetchGifs();
-
-    // useEffect( () => {    
-    //     getGifs( category ).then( ( images ) => { setimages( images ) })
-    // }, [ category ]);
+  const { data:images, loading } = useFetchGifs( category );
 
   return (
       <>
         <h3>{ category }</h3>
 
-        
+        { loading && <p className="animate__animated animate__lightSpeedOutLeft">Cargando...</p> }
 
-        { loading ? 'Cargando...' : 'Data cargada' }
-
-        {/* <div className="card-grid">
-                {
-                    // images.map( ( { id, title } ) => <li key={ id } >{ title }</li> )
-                    images.map( ( img ) => <GifGridItem key={ img.id } { ...img } /> )
-                }
-        </div> */}
+        <div className="card-grid animate__animated animate__fadeInUp">
+            { 
+              images.map( ( img ) => <GifGridItem key={ img.id } { ...img } /> ) 
+            }
+          </div>
       </>
   )
 }
--- curso-u-react-cero-experto/04-gif-expert-app/src/hooks/useFetchGifs.js
+++ curso-u-react-cero-experto/04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,13 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getGifs } from '../helpers/getGifs';
 
-
-export const useFetchGifs = () => {
+export const useFetchGifs = ( category ) => {
 
     const [state, setState] = useState( {
         data:[],
         loading:true
-    })
+    } );
+
+    useEffect( ( ) => {
+        getGifs( category ).then( (images) => {
+            setState({
+                data:images,
+                loading: false
+            })
+        });
+    }, [ category ]);
 
     return state;
-
 }
