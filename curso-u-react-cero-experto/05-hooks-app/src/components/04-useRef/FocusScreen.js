96697ee9ccd7895792e3c8024775b5c48826a323
BlackRoadCode <blackroadcode@gmail.com>
2022-04-20 08:41:51 -0500

    update section 9

--- curso-u-react-cero-experto/05-hooks-app/src/components/04-useRef/FocusScreen.js
+++ curso-u-react-cero-experto/05-hooks-app/src/components/04-useRef/FocusScreen.js
@@ -1,7 +1,24 @@
+import { useRef } from 'react';
 import './ref.css';
 
 export const FocusScreen = () => {
+
+    const inputRef = useRef();
+
+    
+    const handleClick = () => {
+        inputRef.current.select();
+        console.log(inputRef);
+    }
+
   return (
-    <h1>FocusScreen</h1>
+    <>
+        <h1>FocusScreen</h1>
+        <hr />
+
+        <input ref={ inputRef } className='form-control' placeholder='Su nombre' />
+
+        <button className='btn btn-outline-primary mt-3' onClick={ handleClick } >Focus</button>
+    </>
   )
 }
