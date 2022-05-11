c983cc499a603ee33f1be8ccd65f7b21f2f328c1
BlackRoadCode <blackroadcode@gmail.com>
2022-05-11 09:15:12 -0500

    update course

--- curso-u-clean-solid/base-course/src/clean-code/03-functions.ts
+++ curso-u-clean-solid/base-course/src/clean-code/03-functions.ts
@@ -1,22 +1,29 @@
 (() => {
 
+    interface MovieProps{
+        cast: string[],
+        description: string, 
+        rating: number, 
+        title: string,
+    }
+
     // función para obtener información de una película por Id
     function getMovieById( movieId: string ) {
         console.log({ movieId });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getMovieCast( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getActorBioById( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
     // Crear una película
-    function createMovie(title: string, description: string, rating: number, cast: string[] ) {
+    function createMovie( { title, description, rating, cast }:MovieProps ) {
         console.log({ title, description, rating, cast });
     }
 
