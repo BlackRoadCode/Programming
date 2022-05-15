7fe2ec12b737c827b5e25b78891524d4b2ae872d
BlackRoadCode <blackroadcode@gmail.com>
2022-05-15 14:29:27 -0500

    end of SRP exercise

--- curso-u-clean-solid/base-course/src/main.ts
+++ curso-u-clean-solid/base-course/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css';
 // import './clean-code/04-homework';
 // import './clean-code/05-dry';
-import './clean-code/07-tarea';
+// import './clean-code/07-tarea';
+import './solid/01-srp';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
