36b639cef96b944ea7fbbbb8af1e0e88a3e38cdc
BlackRoadCode <blackroadcode@gmail.com>
2022-05-15 19:49:00 -0500

    LSP exercise

--- curso-u-clean-solid/base-course/src/main.ts
+++ curso-u-clean-solid/base-course/src/main.ts
@@ -2,7 +2,8 @@ import './style.css';
 // import './clean-code/04-homework';
 // import './clean-code/05-dry';
 // import './clean-code/07-tarea';
-import './solid/02-open-close-a'
+// import './solid/02-open-close-a'
+import './solid/03-liskov-a'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
