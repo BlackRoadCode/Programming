7c8472b44975c6cc228ff26263ac7ead08e397e0
BlackRoadCode <blackroadcode@gmail.com>
2023-02-16 09:54:10 -0600

    finished QR generator functions

--- ps-utilities/assets/js/qr.js
+++ ps-utilities/assets/js/qr.js
@@ -1,38 +1,42 @@
+
 let img = "assets/img/mandreaqr2.png"; // mandreaqr.jpg
 let canvas = new Object( { name: '' } );
-let qrCodeCanvas = new QRCodeStyling({
-    width: 400,
-    height: 400,
-    data: qrUrl,
-    image: img,
-    qrOptions: {
-        typeNumber: 0,
-        mode: "Byte",
-        errorCorrectionLevel: "Q"
-    },
-    dotsOptions: {
-        color: "#000",
-        type: "square"
-    },
-    cornersSquareOptions: {
-        color: "#000",
-        type: "square"
-    },
-    backgroundOptions: {
-        color: "#fff",
-    },
-    imageOptions: {
-        crossOrigin: "anonymous",
-        margin: 0
-    }
-});
+let qrCodeCanvas = new QRCodeStyling({});
 
 const generateQRCode = ( imageName, qrUrl ) => {
     canvas.name = imageName;
-    qrCodeCanvas.data = qrUrl;
-    qrCodeCanvas.append( document.getElementById( "canvas" ) );
+    qrCodeCanvas._options.data = qrUrl;
+
+    qrCodeCanvas = new QRCodeStyling({
+        width: 400,
+        height: 400,
+        data: qrUrl,
+        image: img,
+        qrOptions: {
+            typeNumber: 0,
+            mode: "Byte",
+            errorCorrectionLevel: "Q"
+        },
+        dotsOptions: {
+            color: "#000",
+            type: "square"
+        },
+        cornersSquareOptions: {
+            color: "#000",
+            type: "square"
+        },
+        backgroundOptions: {
+            color: "#fff",
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: 0
+        }
+    });
+
+    qrCodeCanvas.append( document.getElementById("canvas") );
 }
 
-function downloadQRCodes() {
+const downloadQRCodes = () => {
     qrCodeCanvas.download(canvas);
 }
