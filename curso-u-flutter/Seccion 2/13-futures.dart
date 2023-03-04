011ae6a7b796c9af7d0d721f9541f12176e859ed
BlackRoadCode <blackroadcode@gmail.com>
2023-03-03 18:28:28 -0600

    added futures class

---
+++ curso-u-flutter/Seccion 2/13-futures.dart
@@ -0,0 +1,23 @@
+void main(){
+
+  print('Antes de la petición');
+  
+  httpGet('https://rickymortiapp.com')
+    .then( (data){
+      print( data.toUpperCase() );
+    });
+  
+  print('Fin del programa');
+  
+}
+
+Future<String> httpGet( String url ){
+  return Future.delayed( Duration(seconds: 3), (){
+    return 'Hola Mundo 3 segundos';
+  });
+}
+
+// Forma abreviada del future (usar una o la otra)
+Future<String> httpGet( String url ){
+  return Future.delayed( Duration(seconds: 3), () => 'Hola Mundo 3 segundos' );
+}
