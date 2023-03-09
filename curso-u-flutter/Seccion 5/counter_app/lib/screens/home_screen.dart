aec2eba07b3f5da8c18381b1ffe673272ac5e0fa
BlackRoadCode <blackroadcode@gmail.com>
2023-03-09 12:42:28 -0600

    course clases updated

--- curso-u-flutter/Seccion 5/counter_app/lib/screens/home_screen.dart
+++ curso-u-flutter/Seccion 5/counter_app/lib/screens/home_screen.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('HomeScreen'),
-      );
-  }
 
+    const fontSize30 = TextStyle( fontSize:30 );
+    
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        title: const Text('HomeScreen'),
+        elevation: 2,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget> [
+            Text("Click's Counter 2", style: fontSize30 ),
+            Text('10', style: fontSize30)
+            ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueGrey,
+        onPressed: (){ print('Hola Mundo'); },
+        child: const Icon(Icons.add, size: 30,),
+        )
+    );
+  }
 }
+
+
+// Text('Click`s Counter 2')
