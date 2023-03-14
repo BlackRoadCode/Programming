340a77d171c843bf988721fd0de69875979d45ef
BlackRoadCode <blackroadcode@gmail.com>
2023-03-14 15:12:38 -0600

    updates

---
+++ curso-u-flutter/Seccion 6/fl_components/lib/screens/listview1_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+
+  final options = const['Fortnite', 'Resident Evil', 'Inside', 'Limbo', 'Fifa 2023'];
+   
+  const Listview1Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview 1'),
+      ),
+      body: ListView(
+        children: [
+          ...options.map( 
+            (e) => ListTile(
+              title: Text(e),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              ), 
+          ).toList(),
+        ],
+      )
+    );
+  }
+}
