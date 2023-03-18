62c42d8184fc4d3971bbf76575e52742dc85aece
BlackRoadCode <blackroadcode@gmail.com>
2023-03-17 19:10:38 -0600

    updated app_routes

--- curso-u-flutter/Seccion 6/fl_components/lib/router/app_routes.dart
+++ curso-u-flutter/Seccion 6/fl_components/lib/router/app_routes.dart
@@ -25,15 +25,6 @@ class AppRoutes{
     return appRoutes;
   }
 
-
-  // static Map<String, Widget Function(BuildContext)> routes = {
-  //       'home':( BuildContext context ) => const HomeScreen(),
-  //       'listView1':( BuildContext context ) => const Listview1Screen(),
-  //       'listView2':( BuildContext context ) => const Listview2Screen(),
-  //       'alert':( BuildContext context ) => const AlertScreen(),
-  //       'card':( BuildContext context ) => const CardScreen(),
-  // };
-
   static Route<dynamic> onGenerateRoute( RouteSettings settings) {
         return MaterialPageRoute(
           builder: (context) => const Listview2Screen()
