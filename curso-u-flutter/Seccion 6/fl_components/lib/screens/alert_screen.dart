b6a73e1bcf5ee892e065b69ee2bb42fda5b23d5f
BlackRoadCode <blackroadcode@gmail.com>
2023-03-23 10:50:41 -0600

    finished alert function in Android

--- curso-u-flutter/Seccion 6/fl_components/lib/screens/alert_screen.dart
+++ curso-u-flutter/Seccion 6/fl_components/lib/screens/alert_screen.dart
@@ -12,9 +12,7 @@ class AlertScreen extends StatelessWidget {
         ),
         body: Center(
           child: ElevatedButton(
-            onPressed: () {
-              
-            },
+            onPressed: () => displayDialogAndroid(context),
             child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 13),
               child: Text('Mostrar Alerta', style: TextStyle(fontSize: 17),),
@@ -28,5 +26,36 @@ class AlertScreen extends StatelessWidget {
           child: const Icon(Icons.close)
         ),
     );
+  }
+
+  displayDialogAndroid( BuildContext context ){
+
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: ( context ){
+        return AlertDialog(
+          elevation: 5,
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10) ),
+          semanticLabel: 'Este es semantical label',
+          title: const Text('Título del alert'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox( height: 10, ),
+              FlutterLogo(size: 50,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Cancelar')
+            )
+          ],
+        );
+      }
+    );
+    
   }
 }
