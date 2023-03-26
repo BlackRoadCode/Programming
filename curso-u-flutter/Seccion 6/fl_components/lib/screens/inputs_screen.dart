edd3e9140ff85770c818349002d1eafb2af854e7
BlackRoadCode <blackroadcode@gmail.com>
2023-03-25 20:59:05 -0600

    added mechanism to see the values of the form un console

--- curso-u-flutter/Seccion 6/fl_components/lib/screens/inputs_screen.dart
+++ curso-u-flutter/Seccion 6/fl_components/lib/screens/inputs_screen.dart
@@ -8,6 +8,16 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name' : 'Pepe Sosa',
+      'email'      : 'mail@mail.com',
+      'password'   : 'abc123',
+      'role'       : 'admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -17,16 +27,44 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric( horizontal: 20, vertical: 10 ),
-          child: Column(
-            children: const [
-              CustomTextField( labelText: 'Nombre', helperText: 'Mínimo 3 caracteres', hintText: 'Ingresa tu nombre', icon: Icons.person, suffixIcon: Icons.person_add_alt_1,),
-              SizedBox( height: 30, ),
-              CustomTextField( labelText: 'Email', helperText: 'Ingresa un email válido', hintText: 'Password', icon: Icons.email),
-              SizedBox( height: 30, ),
-              CustomTextField( labelText: 'Password', helperText: 'Mínimo 3 caracteres 2 mayusculas un número', hintText: 'Password', icon: Icons.password_sharp),
-            ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                const SizedBox( height: 30, ),
+                CustomTextField( 
+                  labelText: 'Nombre', 
+                  helperText: 'Mínimo 3 caracteres', 
+                  hintText: 'Ingresa tu nombre', 
+                  icon: Icons.person, 
+                  suffixIcon: Icons.person_add_alt_1, 
+                  formProperty: 'first_name', 
+                  formValues: formValues,),
+                
+                const SizedBox( height: 30, ),
+                CustomTextField( labelText: 'Email', helperText: 'Ingresa un email válido', hintText: 'Correo', icon: Icons.email, textInputType: TextInputType.emailAddress,  formProperty: 'email', formValues: formValues,),
+                
+                const SizedBox( height: 30, ),
+                CustomTextField( labelText: 'Password', helperText: 'Mínimo 3 caracteres 2 mayusculas un número', hintText: 'Password', icon: Icons.password_sharp, obscureText: true, obscuringCharacter: '*',  formProperty: 'password', formValues: formValues, ),
+                
+                const SizedBox( height: 30 ),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus( FocusNode() );
+
+                    if ( !myFormKey.currentState!.validate() ) {
+                      print('Formulario no válido');
+                      return;
+                    }
+
+                    print(formValues);
+                  }, 
+                  child: const Center( child: Text('Manda formulario') )
+                )
+              ],
+            ),
           ),
-          ),
+        ),
       )
     );
   }
--- curso-u-flutter/Seccion 6/fl_components/lib/widgets/custom_textt_field.dart
+++ curso-u-flutter/Seccion 6/fl_components/lib/widgets/custom_textt_field.dart
@@ -2,24 +2,47 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   
-  final String? hintText;
-  final String? labelText;
-  final String? helperText;
+  final bool obscureText;
   final IconData? icon;
   final IconData? suffixIcon;
+  final String obscuringCharacter;
+  final String? helperText;
+  final String? hintText;
+  final String? labelText;
+  final TextInputType? textInputType;
+
+  final String formProperty;
+  final Map<String, String> formValues;
 
   const CustomTextField({
     super.key, 
-    this.hintText, 
-    this.labelText, 
     this.helperText, 
+    this.hintText, 
     this.icon, 
-    this.suffixIcon,
+    this.labelText, 
+    this.obscureText = false, 
+    this.obscuringCharacter = '•',
+    this.suffixIcon, 
+    this.textInputType, 
+    required this.formProperty, 
+    required this.formValues, 
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      autofocus: false,
+      keyboardType: textInputType,
+      obscureText: obscureText,
+      obscuringCharacter: obscuringCharacter,
+      initialValue: '',
+      textCapitalization: TextCapitalization.words,
+      onChanged: (value) => formValues[formProperty] = value,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) {
+        if( value == null ) return 'Este campo es requerido';
+        return value.length < 3 ? 'Mínimo 3 caracteres' : null;
+      },
       decoration: InputDecoration(
         icon: icon == null ? null : Icon( icon ),
         suffixIcon: suffixIcon == null ? null : Icon( suffixIcon ),
@@ -27,17 +50,6 @@ class CustomTextField extends StatelessWidget {
         labelText: labelText,
         helperText: helperText,
       ),
-      autofocus: false,
-      initialValue: '',
-      textCapitalization: TextCapitalization.words,
-      onChanged: (value) {
-        print('value: $value');
-      },
-      validator: (value) {
-        if( value == null ) return 'Este campo es requerido';
-        return value.length < 3 ? 'Mínimo 3 caracteres' : null;
-      },
-      autovalidateMode: AutovalidateMode.onUserInteraction,
     );
   }
 }
