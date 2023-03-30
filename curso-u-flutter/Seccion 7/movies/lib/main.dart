b702355c534fc3be43b6515a020246ff89ff412e
BlackRoadCode <blackroadcode@gmail.com>
2023-03-30 17:53:25 -0600

    finished http conection

--- curso-u-flutter/Seccion 7/movies/lib/main.dart
+++ curso-u-flutter/Seccion 7/movies/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:movies/screens/screens.dart';
+import 'package:movies/providers/movies_provider.dart';
 import 'package:movies/themes/app_theme.dart';
+import 'package:movies/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,6 +21,20 @@ class MyApp extends StatelessWidget {
         'home':  ( _ ) => const HomeScreen(),
         'details':  ( _ ) => const DetailsScreen(),
       },
+    );
+  }
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider( create: ( _ ) => MoviesProvider(), lazy: false )
+      ],
+      child: const MyApp(),
     );
   }
 }
--- curso-u-flutter/Seccion 7/movies/lib/providers/movies_provider.dart
+++ curso-u-flutter/Seccion 7/movies/lib/providers/movies_provider.dart
@@ -1,5 +1,8 @@
 
-class MoviesProvider{
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MoviesProvider extends ChangeNotifier{
 
   MoviesProvider(){
     print('movies provider inicializado');
@@ -7,8 +10,11 @@ class MoviesProvider{
     getOnDisplayMovies();
   }
 
-  getOnDisplayMovies(){
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https('api.themoviedb.org', '/3/movie/now_playing', { 'api_key': 'f3d2f08df9ec3917075a079ab6b6b2d3', 'language':'es-ES', 'page':'1' });
+    var response = await http.get(url);
+
+    print('$response.body');
   }
 
 }
