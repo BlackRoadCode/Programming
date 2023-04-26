286852ff11dc292732116f04dbc7edb10ba83350
BlackRoadCode <blackroadcode@gmail.com>
2023-04-25 19:04:58 -0600

    Added circular progress view

--- curso-dt-flutter/Seccion 9/widgets_app/lib/presentation/screens/progress/progress_screen.dart
+++ curso-dt-flutter/Seccion 9/widgets_app/lib/presentation/screens/progress/progress_screen.dart
@@ -10,11 +10,44 @@ class ProgressScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-           title: const Text('ProgressTitle'),
+           title: const Text('Progress Indicators'),
        ),
-      body: const Center(
-         child: Text('ProgressScreen'),
-      ),
+      body: const _ProgressView(),
     );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+       child: Column(
+        children: const [
+          SizedBox( height: 30 ),
+
+          Text('Circular Progress Indicator'),
+          SizedBox( height: 15 ),
+          CircularProgressIndicator( strokeWidth: 2, backgroundColor: Colors.black45 ),
+
+          SizedBox( height: 20 ),
+
+          Text('Progress Indicator controlado'),
+          SizedBox( height: 15 ),
+          _ControllerProgressIndicator(),
+
+        ],
+       ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
