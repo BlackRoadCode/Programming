55a409bbcf36335209aa81fecbd51aae97b5a92b
BlackRoadCode <blackroadcode@gmail.com>
2023-04-30 14:21:21 -0600

    End of section 10 - end of infinite scroll screen and functions

--- curso-dt-flutter/Seccion 9/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ curso-dt-flutter/Seccion 9/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,8 +14,84 @@ class InfiniteScrollScreen extends StatefulWidget {
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
 
-  List<int> imagesIds = [ 30, 31, 32, 33, 34, 35 ];
+  List<int> imagesIds = [ 1, 2, 3, 4, 5 ];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
 
+  void addFiveImages(){
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll(
+      [1, 2, 3, 4, 5].map((e) => lastId + e)
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() { 
+      if( (scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent ){
+        loadNextPage();
+      }
+    });
+    
+  }
+
+  Future loadNextPage() async {
+
+    if( isLoading ) return;
+    isLoading = true;
+    setState((){});
+
+    await Future.delayed( const Duration( seconds: 2 ) );
+
+    addFiveImages();
+
+    isLoading = false;
+
+    if( !isMounted ) return;
+
+    setState((){});
+
+    moveScrollToBottom();
+
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed( const Duration( seconds: 2 ) );
+    if( !isMounted ) return;
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+    setState((){});
+  }
+
+  void moveScrollToBottom(){
+    if( scrollController.position.pixels + 150 <= scrollController.position.maxScrollExtent ) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120, 
+      duration: const Duration( milliseconds: 400 ), 
+      curve: Curves.fastOutSlowIn
+    );
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,24 +100,35 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          physics: const BouncingScrollPhysics(),
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-              placeholder: const AssetImage('assets/img/jar-loading.gif'), 
-              image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300')
-            );
-          },
+        child: RefreshIndicator(
+          strokeWidth: 2,
+          onRefresh: onRefresh,
+          child: ListView.builder(
+            controller: scrollController,
+            physics: const BouncingScrollPhysics(),
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+                placeholder: const AssetImage('assets/img/jar-loading.gif'), 
+                image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300')
+              );
+            },
+          ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.arrow_back_ios_new_sharp ),
-        onPressed: () => context.pop()
+        onPressed: 
+          isLoading 
+            ? null
+            : () => context.pop(),
+        child: 
+          isLoading 
+            ? const CircularProgressIndicator()
+            : FadeIn( duration: const Duration( milliseconds: 600 ), child: const Icon( Icons.arrow_back_ios_new_sharp )),
       ),
     );
   }
