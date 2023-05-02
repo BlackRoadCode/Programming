e21e0abf8e6586bb69a040348c4725fc89bd9ec6
BlackRoadCode <blackroadcode@gmail.com>
2023-05-02 09:41:20 -0600

    start cinemapedia project and section 12 with a base app

---
+++ curso-dt-flutter/Seccion 12/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const name = 'home_screen'; 
+   
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+           title: const Text('HomeTitle'),
+       ),
+      body: const Center(
+         child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
