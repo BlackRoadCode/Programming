e067a59499aef46b8a3bbb4102f88b8a720b9a89
BlackRoadCode <blackroadcode@gmail.com>
2024-02-18 19:15:39 -0600

    update end of section 3

--- curso-dt-node/02-bases/src/app.js
+++ curso-dt-node/02-bases/src/app.js
@@ -1,16 +1,28 @@
 
+const { getAge, getUUID } = require('./plugins');
+// const { buildMakePerson } = require('./js-foundation/05-factory');
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 // const { getUserById } = require('./js-foundation/03-callbacks');
-const { getUserById } = require('./js-foundation/04-arrow');
+// const { getUserById } = require('./js-foundation/04-arrow');
 
-// console.log( emailTemplate );
+const {getPokemonById, getAlternativeDataByCode} = require('./js-foundation/06-promises');
 
-const id = 1;
-
-getUserById( id, ( err, user ) => {
-    if( err ){
-        throw new Error( err )
-    }
-    console.log( user );
+getPokemonById(4).then( (pokemon) =>{
+    console.log({pokemon});
 });
+
+// getAlternativeDataByCode(3354483).then( (data) => console.log(data));
+
+//! Referencia a la función factory y uso
+// const makePerson = buildMakePerson({getAge, getUUID});
+// const obj = { name: 'Juan Perez', birthdate: '1982-12-24' };
+// const pepe = makePerson( obj );
+// console.log({ pepe });
+
+//* kajshdasjkdhaskjda
+
+// todo alskjdsakldjalksdjaks
+
+// ? kashdjkasdhasjkdhjkasd
+
