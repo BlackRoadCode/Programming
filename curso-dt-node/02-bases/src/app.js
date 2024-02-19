86b862e9d61f42f6c38035e6c88861d5cff8f1a6
BlackRoadCode <blackroadcode@gmail.com>
2024-02-19 10:24:13 -0600

    end of proyect 02, start a typescript project 03

--- curso-dt-node/02-bases/src/app.js
+++ curso-dt-node/02-bases/src/app.js
@@ -1,16 +1,23 @@
 
-const { getAge, getUUID } = require('./plugins');
+const { getAge, getUUID, buildLogger } = require('./plugins');
 // const { buildMakePerson } = require('./js-foundation/05-factory');
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 // const { getUserById } = require('./js-foundation/03-callbacks');
 // const { getUserById } = require('./js-foundation/04-arrow');
 
-const {getPokemonById, getAlternativeDataByCode} = require('./js-foundation/06-promises');
+// const {getPokemonById, getAlternativeDataByCode} = require('./js-foundation/06-promises');
 
-getPokemonById(4).then( (pokemon) =>{
-    console.log({pokemon});
-});
+// getPokemonById(4)
+//     .then( (pokemon) => console.log({pokemon}))
+//     .catch( ( err ) => console.error(err) )
+//     .finally( () => console.log('Finalizado') );
+
+const logger = buildLogger('app.js');
+
+logger.log('Hola Mundo');
+logger.error('Este es un error');
+
 
 // getAlternativeDataByCode(3354483).then( (data) => console.log(data));
 
