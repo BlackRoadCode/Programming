190e0118c863f10988ec96f99cd45d22cedd7a3c
BlackRoadCode <blackroadcode@gmail.com>
2024-03-04 20:35:03 -0600

    end of sections 1 and 2, basics

---
+++ curso-u-openai-node/01-basics/src/index.ts
@@ -0,0 +1,39 @@
+import { OpenAI } from "openai";
+import { encoding_for_model } from "tiktoken";
+
+const openai = new OpenAI({});
+
+async function main(){
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: 'system',
+                content: 'You respond like a cool bro'
+            },
+            {
+                role: 'user',
+                content: 'Who tall is michael jordan'
+            }
+        ],
+        max_tokens: 100,
+        n:2
+    });
+
+    console.log(response);
+    console.log(response.choices[0].message);
+    console.log(response.choices[0].message.content);
+}
+
+function encodePrompt(){
+
+    const prompt = 'How tall is the mount kilimanjaro';
+    const encoder = encoding_for_model("gpt-3.5-turbo");
+    const words = encoder.encode(prompt);
+
+    console.log(words);    
+
+}
+
+main();
+// encodePrompt();
