6fec6a67d99b320b2d0467c466296214f455b05d
BlackRoadCode <blackroadcode@gmail.com>
2024-06-05 20:35:46 -0600

    Limpiar el caché de Git y añadir todos los cambios

---
+++ curso-dt-nest/02-car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        {
+          id: 1,
+          brand: 'Audi',
+          model: 'A3'
+        },
+        {
+          id: 2,
+          brand: 'BMW',
+          model: 'X5'
+        },
+        {
+          id: 3,
+          brand: 'Mercedes-Benz',
+          model: 'C-Class'
+        },
+        {
+          id: 4,
+          brand: 'Tesla',
+          model: 'Model S'
+        },
+        {
+          id: 5,
+          brand: 'Toyota',
+          model: 'Corolla'
+        },
+        {
+          id: 6,
+          brand: 'Ford',
+          model: 'Mustang'
+        },
+        {
+          id: 7,
+          brand: 'Chevrolet',
+          model: 'Camaro'
+        }
+    ];
+
+    findAll(){
+        return this.cars;
+    }
+
+    findOneById( id:number ){
+        return this.cars.find( car => car.id === id );
+    }
+      
+}
