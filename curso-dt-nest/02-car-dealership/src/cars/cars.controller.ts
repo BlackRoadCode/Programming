8c5b9f01db722ceac6af3a5260a9d25c0ea61989
BlackRoadCode <blackroadcode@gmail.com>
2024-06-09 15:06:42 -0600

    updated files from section 3}

--- curso-dt-nest/02-car-dealership/src/cars/cars.controller.ts
+++ curso-dt-nest/02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -12,8 +12,29 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id ){
-        return this.carsService.findOneById( Number(id) );
+    getCarById( @Param( 'id', ParseIntPipe ) id:number ){
+        console.log( {id} );
+        return this.carsService.findOneById( id );
+    }
+
+    @Post()
+    createCar( @Body() body:any ){
+        return body
+    }
+    
+    @Patch(':id')
+    upadteCar( 
+        @Param( 'id', ParseIntPipe ) id:number, 
+        @Body() body:any ) {
+        return {body, id}
+    }
+    
+    @Delete(':id')
+    delteCar( @Param('id', ParseIntPipe) id:number ){
+        return {
+            method: 'delete',
+            id
+        };
     }
 
 }
