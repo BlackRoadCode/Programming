9dcc5b537a3d15f67d6decf3721fb154f50bb536
BlackRoadCode <blackroadcode@gmail.com>
2024-06-24 11:51:58 -0600

    end of section 8

--- curso-dt-nest/03-pokedex/src/seed/seed.service.ts
+++ curso-dt-nest/03-pokedex/src/seed/seed.service.ts
@@ -1,37 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
   constructor( 
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon> 
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
   ){}
-
-  private readonly axios:AxiosInstance = axios;
   
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
 
-    const numPokemones = 1;
-    const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${ numPokemones }`);
-    // const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${ numPokemones }&offset=${ numPokemones }`);
+    const numPokemones = 650;
+    const data = await this.http.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${ numPokemones }`);
+    const pokemonToInsert: { name: string, no: number }[] = [];
 
-    data.results.forEach( ({ name, url }) => {
+    data.results.forEach( async ({ name, url }) => {
       const segments = url.split('/');
       const no:number = +segments[ segments.length - 2 ];
-
-      this.pokemonModel.create({ name, no });
-
+      pokemonToInsert.push( { name, no } ); 
     });
 
+    this.pokemonModel.insertMany( pokemonToInsert );
     return 'Seed executed';
-
   }
 
 }
