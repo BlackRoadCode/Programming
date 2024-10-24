cb5ad0ab20089faf3c7a871397092abcdee3136a
BlackRoadCode <blackroadcode@gmail.com>
2024-10-23 20:45:42 -0600

    deleted 03 first version proyect (code deprecated)

--- curso-u-react-cero-experto/03-counter-app/src/base/02-template-string.js
+++
@@ -1,9 +0,0 @@
-
-const name = 'Pepe';
-const lastName = 'Sosa';
-
-const completeName = `${ name } ${ lastName }`;
-
-export function getSaludo( nombre = 'Carlos' ){
-    return `Hola ${ nombre }`;
-}
